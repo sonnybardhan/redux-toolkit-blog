b8970c0d6ea7bb6c5a08346c935aba18945ff94a
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-09-16 12:14:00 +0530

    implement createAsyncThunk to fetchPosts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,51 +1,24 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { sub } from 'date-fns';
 
-const initialState = [
-  {
-    id: 1,
-    title: 'Post #1',
-    content: 'Some content here ... ',
-    createdAt: sub(new Date(), { minutes: 30 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      thumbsDown: 0,
-      wow: 0,
-      heart: 0,
-    },
-  },
-  {
-    id: 2,
-    title: 'Post #2',
-    content: 'More content here ... ',
-    createdAt: sub(new Date(), { minutes: 20 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      thumbsDown: 0,
-      wow: 0,
-      heart: 0,
-    },
-  },
-  {
-    id: 3,
-    title: 'Post #3',
-    content: 'Even more content here ... ',
-    createdAt: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      thumbsDown: 0,
-      wow: 0,
-      heart: 0,
-    },
-  },
-];
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
-// const reactionEmojis = {
-//   thumbsUp: '👍',
-//   thumbsDown: '👎',
-//   wow: '🤩',
-//   heart: '❤️',
-// };
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null,
+};
+
+const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  try {
+    const response = await axios.get(POSTS_URL);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -53,7 +26,7 @@ const postsSlice = createSlice({
   reducers: {
     createPost: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -76,15 +49,42 @@ const postsSlice = createSlice({
     addReaction: {
       reducer(state, action) {
         const { postId, reaction } = action.payload;
-        const post = state.find((post) => post.id === postId);
+        const post = state.posts.find((post) => post.id === postId);
         if (post) {
           post.reactions[reaction] += 1;
         }
       },
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        let min = 1;
+        const retrievedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            thumbsDown: 0,
+            wow: 0,
+            heart: 0,
+          };
+          return post;
+        });
+        state.posts = state.posts.concat(retrievedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.status;
+export const getPostsError = (state) => state.error;
 export const { createPost, addReaction } = postsSlice.actions;
 export default postsSlice.reducer;
